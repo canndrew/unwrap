650be882d4e466159d11ac1f240fa6eea0b5b2ae
Andrew Cann <shum@canndrew.org>
2016-03-08 18:36:14 +0800

    Export macro. Add docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,30 @@
 use std::fmt::Debug;
 
+/// Types which can be unwrapped and which may want to print a verbose error message when they are
+/// unwrapped incorrectly. This trait is implemented for `Result` and `Option` as a replacement for
+/// their inherent `unwrap` methods. This trait is intended to be used via this crate's `unwrap!`
+/// macro.
 pub trait VerboseUnwrap {
+    /// The wrapped type.
     type Wrapped;
 
+    /// Unwrap the value into it's inner type or panicks with an error message when the value
+    /// cannot be unwrapped. This method is intended to be called via this crate's `unwrap!` macro.
+    ///
+    /// # Panics
+    ///
+    /// When the value cannot be unwrapped. Eg. on an `Err` or `None` value.
+    ///
+    /// # Arguments
+    ///
+    /// These arguments are used to print a useful diagnostic when the method panicks.
+    /// 
+    ///  * `message`: An optional message, printed alongside the rest of the info.
+    ///  * `module_path`: The module path where this method is being called from. Eg.
+    ///    `my_crate::my_module::my_function`
+    ///  * `file`: The filename where this method is being called from.
+    ///  * `line_number`: The line number where this method is being called from.
+    ///  * `column`: The column number where this method is being called from
     fn verbose_unwrap(self, message: Option<&str>, module_path: &str, file: &str, line_number: u32, column: u32) -> Self::Wrapped;
 }
 
@@ -90,6 +112,25 @@ impl<T> VerboseUnwrap for Option<T> {
     }
 }
 
+/// A replacement for calling `unwrap()` on a `Result` or `Option`.
+///
+/// This macro is intended to be used in all cases where one would `unwrap` a `Result` or `Option`
+/// to deliberately panic in case of error, e.g. in test-cases. Such `unwrap`s don't give a precise
+/// point of failure in the code and instead indicate some line number in the Rust core library.
+/// This macro provides a precise point of failure and decorates the failure for easy viewing.
+///
+/// # Examples
+///
+/// ```
+/// # #[macro_use]
+/// # extern crate unwrap;
+/// # fn main() {
+/// let some_option = Some("Hello".to_string());
+/// let string_length = unwrap!(some_option, "This is an optional user-supplied text.").len();
+/// assert_eq!(string_length, 5);
+/// # }
+/// ```
+#[macro_export]
 macro_rules! unwrap(
     ($e:expr) => (
         $crate::VerboseUnwrap::verbose_unwrap($e, None, module_path!(), file!(), line!(), column!())
