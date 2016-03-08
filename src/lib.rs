c63e9be82d76a7487996c8290369422021b57b64
Andrew Cann <shum@canndrew.org>
2016-03-08 18:17:10 +0800

    Add optional message

--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,13 @@ use std::fmt::Debug;
 pub trait VerboseUnwrap {
     type Wrapped;
 
-    fn verbose_unwrap(self, module_path: &str, file: &str, line_number: u32, column: u32) -> Self::Wrapped;
+    fn verbose_unwrap(self, message: Option<&str>, module_path: &str, file: &str, line_number: u32, column: u32) -> Self::Wrapped;
 }
 
 impl<T, E: Debug> VerboseUnwrap for Result<T, E> {
     type Wrapped = T;
 
-    fn verbose_unwrap(self, module_path: &str, file: &str, line_number: u32, column: u32) -> T {
+    fn verbose_unwrap(self, message: Option<&str>, module_path: &str, file: &str, line_number: u32, column: u32) -> T {
         match self {
             Ok(t) => t,
             Err(e) => {
@@ -26,14 +26,32 @@ impl<T, E: Debug> VerboseUnwrap for Result<T, E> {
                     }
                 }
                 */
-                
-                panic!("\n\
+               
+                match message {
+                    Some(m) => {
+                        panic!("\n\
+\n\
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\
 !   unwrap! called on Result::Err                                              !\n\
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\
 {}:{},{} in {}\n\
-{:#?}",
-                    file, line_number, column, module_path, e);
+{}\n\
+\n\
+{:#?}\n\
+\n", file, line_number, column, module_path, m, Err::<(), E>(e));
+                    },
+                    None => {
+                        panic!("\n\
+\n\
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\
+!   unwrap! called on Result::Err                                              !\n\
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\
+{}:{},{} in {}\n\
+\n\
+{:#?}\n\
+\n", file, line_number, column, module_path, Err::<(), E>(e));
+                    },
+                }
             },
         }
     }
@@ -42,25 +60,43 @@ impl<T, E: Debug> VerboseUnwrap for Result<T, E> {
 impl<T> VerboseUnwrap for Option<T> {
     type Wrapped = T;
 
-    fn verbose_unwrap(self, module_path: &str, file: &str, line_number: u32, column: u32) -> T {
+    fn verbose_unwrap(self, message: Option<&str>, module_path: &str, file: &str, line_number: u32, column: u32) -> T {
         match self {
             Some(t) => t,
             None => {
-                panic!("\n\
+                match message {
+                    Some(m) => {
+                        panic!("\n\
+\n\
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\
 !   unwrap! called on Option::None                                             !\n\
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\
-{}:{},{} in {}",
-                    file, line_number, column, module_path);
+{}:{},{} in {}\n\
+{}\n\
+\n", file, line_number, column, module_path, m);
+                    },
+                    None => {
+                        panic!("\n\
+\n\
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\
+!   unwrap! called on Option::None                                             !\n\
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\
+{}:{},{} in {}\n\
+\n", file, line_number, column, module_path);
+                    },
+                }
             },
         }
     }
 }
 
 macro_rules! unwrap(
-    ($e:expr) => {
-        $crate::VerboseUnwrap::verbose_unwrap($e, module_path!(), file!(), line!(), column!())
-    }
+    ($e:expr) => (
+        $crate::VerboseUnwrap::verbose_unwrap($e, None, module_path!(), file!(), line!(), column!())
+    );
+    ($e:expr, $message:expr) => (
+        $crate::VerboseUnwrap::verbose_unwrap($e, Some($message), module_path!(), file!(), line!(), column!())
+    );
 );
 
 #[cfg(test)]
@@ -69,12 +105,21 @@ mod tests {
     fn unwrap_result_ok() {
         let result: Result<u32, u32> = Ok(32);
         let x = unwrap!(result);
+        let y = unwrap!(result, "Here's a message");
         assert_eq!(x, 32);
+        assert_eq!(y, 32);
     }
 
     #[test]
     #[should_panic]
-    fn unwrap_result_err() {
+    fn unwrap_result_err_message() {
+        let result: Result<u32, u32> = Err(32);
+        let _ = unwrap!(result, "Here's a message");
+    }
+
+    #[test]
+    #[should_panic]
+    fn unwrap_result_err_no_message() {
         let result: Result<u32, u32> = Err(32);
         let _ = unwrap!(result);
     }
@@ -83,12 +128,21 @@ mod tests {
     fn unwrap_result_some() {
         let option: Option<u32> = Some(32);
         let x = unwrap!(option);
+        let y = unwrap!(option, "Here's a message");
         assert_eq!(x, 32);
+        assert_eq!(y, 32);
     }
 
     #[test]
     #[should_panic]
-    fn unwrap_result_none() {
+    fn unwrap_result_none_message() {
+        let option: Option<u32> = None;
+        let _ = unwrap!(option, "Here's a message");
+    }
+
+    #[test]
+    #[should_panic]
+    fn unwrap_result_none_no_message() {
         let option: Option<u32> = None;
         let _ = unwrap!(option);
     }
